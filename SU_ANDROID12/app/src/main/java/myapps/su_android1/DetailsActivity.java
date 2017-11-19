3c2ba66accd7df702aecc1e0d40bbdf90fd1a42d
GME01 <guillaume.meneroud@etu.univ-lyon1.fr>
2017-11-19 10:30:27 +0100

    test

---
+++ SU_ANDROID12/app/src/main/java/myapps/su_android1/DetailsActivity.java
@@ -0,0 +1,51 @@
+package myapps.su_android1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Guillaume on 03/11/2017.
+ */
+
+public class DetailsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.presentation);
+
+        Button CANCEL_Button = findViewById(R.id.cancel);
+        if (CANCEL_Button != null) {
+            CANCEL_Button.setOnClickListener(cancelLayout);
+        }
+        Button OK_Button = findViewById(R.id.ok);
+        if (OK_Button != null) {
+            OK_Button.setOnClickListener(okLayout);
+        }
+
+    }
+
+    private View.OnClickListener cancelLayout = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intentReturn = new Intent (DetailsActivity.this, MainActivity.class);
+            startActivity(intentReturn);
+            finish();
+        }
+    };
+
+    private View.OnClickListener okLayout = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intentReturnok = new Intent (DetailsActivity.this, MainActivity.class);
+            startActivity(intentReturnok);
+            finish();
+        }
+    };
+
+
+
+}
+
---
+++ SU_ANDROID12/app/src/main/java/myapps/su_android1/MainActivity.java
@@ -0,0 +1,32 @@
+package myapps.su_android1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button OK_boubou = findViewById(R.id.button);
+        if (OK_boubou != null) {
+            OK_boubou.setOnClickListener(changeLayout);
+        }
+    }
+
+    private View.OnClickListener changeLayout = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intentDetail = new Intent (MainActivity.this, DetailsActivity.class);
+            startActivity(intentDetail);
+            finish();
+        }
+
+    };
+
+}
